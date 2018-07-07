a891889acbc67020734a0f0e938c571a43d5fd6b
Henry Walton Jones <CarlosRDGZ>
2018-07-06 23:40:38 -0500

    api usuario

--- api/user.js
+++ api/user.js
@@ -1,13 +1,22 @@
 const users = require('express').Router();
 const User = require('../models/User');
+const user = new User();
 
 users.route('/')
   .post((req,res) => {
-    const user = req.body;
-    let badReq = false;
-    ['name','email','token','school','idArea'].forEach(prop => {
-      if (user[prop] == null)
-        res.send()
-    })
-    User.create()
+    try {
+      res.json(user.create(req.body));
+    } catch (err) {
+      res.status(400).json(err);
+    }
   })
+
+user.route('/:id')
+  .put((req,res) => {
+    try {
+      res.json(user.update(req.body, req.params.id)); // talvez podrias comparar que los id sean iguales, I dont know
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  })
+
