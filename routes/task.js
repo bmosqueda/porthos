2edc28d031bc02c093797896f7c23c5064581764
Henry Walton Jones <CarlosRDGZ>
2018-07-07 13:06:58 -0500

    task view

--- routes/task.js
+++ routes/task.js
@@ -1,7 +1,31 @@
 const router = require('express').Router();
+const Task = require('../models/Task');
 
 router.get('/create', (req, res) => {
   res.render('task/create');
+});
+
+router.get('/view/:id', (req, res) => {
+  const rgx = /^\d+$/;
+  const t = new Task();
+  if (rgx.test(req.params.id)) {
+    t.getByIdAllInfo(req.params.id)
+      .then(task => {
+        t.getAllUserTask(req.params.id, task[0].idAuthor)
+        .then(files => {
+          task[0].files = files;
+          console.log(task);
+          res.render('task/view', { task: task[0] });
+        })
+        .catch(err => {
+          res.status(500).json({err: err.message});
+        });
+      })
+      .catch(err => {
+        res.status(500).json({err: err.message});
+      });
+  } else
+    res.status(400).json({err: "Invalid id"});
 });
 
 module.exports = router;
